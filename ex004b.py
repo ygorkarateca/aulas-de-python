5b65570401c89a89f36501a02c4e6e7ea89bc41a
Ygor Fernandes <ygorkarateca@gmail.com>
2022-10-19 12:43:24 -0300

    Update ex004b.py

--- ex004b.py
+++ ex004b.py
@@ -4,4 +4,5 @@
 n = input('Digite algo: ')
 print('É Letra ?', n.isalpha())
 print('É Número ?', n.isalnum())
+print('É Alfanumerico? ', n.isalnum())
 print('Pode ser usando como titulo? ', n.istitle)
