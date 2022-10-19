ab3d388ec802dbc9b0115381ceb8b21dce7aae10
Ygor Fernandes <ygorkarateca@gmail.com>
2022-10-19 14:19:32 -0300

    Alteracao

--- ex005.py
+++ ex005.py
@@ -1,3 +1,7 @@
 #Faca um programa que leia um número inteiro e mostre
 #na tela o seu sucessor e seu antecessor
 
+n1 = int(input('Digite um valor: '))
+suc = n1 + 1
+ant = n1 - 1
+print('O Sucessor é : {}\nE Antecessor é: {}4'.format(suc, ant))
