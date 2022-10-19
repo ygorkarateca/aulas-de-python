e21582de22db3097c1bb8803b9ce5a39f30aec63
Ygor Fernandes <ygorkarateca@gmail.com>
2022-10-19 13:24:16 -0300

    Create au007.py

---
+++ au007.py
@@ -0,0 +1,27 @@
+#Operadores Aritiméticos
+
+"""
++ = Adicao              ** = Potência
+- = Subtracao           // = Divisão Inteiro
+* = Multiplicacao       % = Resto da Divisão
+/ = Divisao
+
+"""
+
+#Ordem de Procedência
+"""
+
+1º = ()    
+2º = **
+3º = * , / , // , % 
+4º = + , -
+
+"""
+#Exemplos de Procedências
+ex1 = 5+3*2
+ex2 = 3*5+4**2
+ex3 = 3*(5+4)**2
+
+print('Valor do exemplo1 é =', ex1)
+print('Valor do exemplo2 é =', ex2)
+print('Valor do exemplo2 é =', ex3)
