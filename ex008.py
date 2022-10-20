1f1d8a43b9f8791e12f04da09fe942e76a59ffb8
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2022-10-20 08:55:23 -0300

    update ex008.py

--- ex008.py
+++ ex008.py
@@ -1,7 +1,7 @@
 #Escreva um programa que leia um valor em metros
 #e o exiba convertido em centimetros e milímetros
 
-medida = float(input('Digite um número: '))
-cm = medida * 100
-mm = medida * 1000
-print('O valor da medida = {}m \nconvertida em {}cm | {}mm'.format(medida, cm, mm))
+medida = float(input('Digite em metros: '))
+print(f'O valor da medida = {medida}m ')
+print(f'Convertidos em {medida * 10}dm | {medida * 100}cm | {medida * 1000}m')
+print(f'{medida / 10}dam | {medida / 100}hm | {medida / 1000}km')
