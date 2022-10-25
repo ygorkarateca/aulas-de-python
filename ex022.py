3e51b7680723ad14717bfe6b41487a0f4ce66e7e
Ygor Fernandes <ygorkarateca@gmail.com>
2022-10-25 13:25:13 -0300

    Create ex022.py

---
+++ ex022.py
@@ -0,0 +1,14 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+"""
+> O nome com todas as letras maiúsculas
+> O nome com todas minúsculas
+> Quantas letras ao todos(Sem considerar espacos)
+> Quantas letras tem o primeiro nome
+
+"""
+
+nome = 'Gichin Funakoshi'
+
+print(f'Exemplo1 -> {nome.upper()}')
+print(f'Exemplo2 -> {nome.lower()}')
+print(f'Exemplo3 -> {len(nome):}')
