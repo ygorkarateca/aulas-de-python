18a9f1934341cdafd7d3b97e78838f9ebf4e70e3
Ygor Fernandes <ygorkarateca@gmail.com>
2022-10-25 12:58:22 -0300

    update

---
+++ Aulas/au009.py
@@ -0,0 +1,42 @@
+frase = 'Clube de Regatas do Flamengo'
+
+#Fatiamentos
+
+"""
+Exemplos 
+
+lista = (frase[1:6])
+1 -> Comecará do C para frente
+6 -> Do ESPACO para trás
+
+lista = (frase[9:28:3])
+9 -> Comecará do BLOCO 9 
+28 -> Vai imprimir do BLOCO 28 para trás
+3 -> Vai pular as frases 3 em 3
+
+lista = (frase[:6])
+6 -> Imprime inicio do bloco e para um número antes do valor escolhido
+
+lista = (frase[17:])
+17 -> Imprime do valor escolhido até o final da lista
+"""
+
+#Análise
+
+"""
+Comando LEN -> Usado para mostrar o cumprimento da lista
+exemplo = print(len(frase))
+
+Comando COUNT -> Usado para mostrar somente o valor selecionado
+exemplo = print(frase.count('a'))
+
+Comando FIND -> Usado para procurar o valor dentro de uma lista
+exemplo = print(frase.find('r'))
+"""
+
+#Transformacao
+
+"""
+Comando REPLACE -> Usado para substituir uma frase dentro da lista
+exemplo = print(frase.replace('Flamengo', 'Mengão'))
+"""
